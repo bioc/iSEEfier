79f82d040b3965394e5909adb1feb9141ac52830
Federico Marini <marinif@uni-mainz.de>
2024-03-06 18:44:51 +0100

    using runPCA from scater in the example and in the test

--- R/iSEEfier.R
+++ R/iSEEfier.R
@@ -14,7 +14,7 @@
 #' @examples
 #' sce <- scRNAseq::RichardTCellData()
 #' sce <- scuttle::logNormCounts(sce)
-#' sce <- BiocSingular::runPCA(sce)
+#' sce <- scater::runPCA(sce)
 #' sce <- scater::runTSNE(sce)
 #' gene_list <- c("ENSMUSG00000026581", "ENSMUSG00000005087", "ENSMUSG00000015437")
 #' cluster <- "stimulus"
