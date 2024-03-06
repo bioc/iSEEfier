b32d8b0f14a34b37478efa12d3cb3ba20b97cb73
Najla Abassi <abassi.nejla96@gmail.com>
2024-03-06 13:20:07 +0100

    update test

--- tests/testthat/test_iSEEfier.R
+++ tests/testthat/test_iSEEfier.R
@@ -1,10 +1,12 @@
 test_that("test iSEEfier",{
   sce <- scRNAseq::RichardTCellData()
   sce <- scuttle::logNormCounts(sce)
-  sce <- BiocSingular::runPCA(sce, ncomponents=5)
+  sce <- BiocSingular::runPCA(sce)
   sce <- scater::runTSNE(sce)
   initial <- iSEEfier(sce = sce,
-                      feature.list = c("ENSMUSG00000026581", "ENSMUSG00000005087"),
+                      feature.list = c("ENSMUSG00000026581", #SELL
+                                       "ENSMUSG00000005087"), #CD44
                       clusters = "stimulus",
                       conditions = "single cell quality")
+  expect_true(class(initial)== "list")
 })
