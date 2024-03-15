6ed6b9819e464374c797e96f2ff91a02ef25053b
Federico Marini <marinif@uni-mainz.de>
2024-03-15 20:14:27 +0100

    documenting the view_init_ functions

--- R/utils.R
+++ R/utils.R
@@ -1,14 +1,33 @@
-#' Title
+#' View an initial object as a set of tiles
 #'
-#' @param initial
+#' Previews the layout of the `initial` configuration object in a graphical form.
 #'
-#' @return
+#' Tiles are used to represent the panel types, and reflect the values of their
+#' width.
+#' This can be a compact visualization to obtain an overview for the configuration,
+#' without the need of fully launching the app and loading the content of all
+#' panels
+#'
+#' @details
+#' This function is particularly useful with mid-to-large `initial` objects, as
+#' they can be quickly generated in a programmatic manner via the `iSEEinit()`
+#' provided in this package.
+#'
+#'
+#' @param initial An `initial` list object, in the format that is required to
+#' be passed as a parameter in the call to [iSEE::iSEE()].
+#'
+#' @return A `ggplot` object, representing a schematic view for the `initial`
+#' object.
+#'
 #' @export
 #'
 #' @importFrom stats na.omit
 #' @importFrom ggplot2 ggplot aes geom_tile scale_fill_manual scale_y_discrete
 #' theme theme_void
 #' @importFrom rlang .data
+#'
+#' @seealso [view_initial_network()]
 #'
 #' @examples
 view_initial_tiles <- function(initial) {
@@ -121,15 +140,38 @@ view_initial_tiles <- function(initial) {
 }
 
 
-#' Title
+#' View an initial object as a network
 #'
-#' @param initial
-#' @param plot_format
+#' Translates the layout of the `initial` configuration object as a networks,
+#' representing panels as nodes and links between them as edges.
 #'
-#' @return
+#' Panels are the nodes, with color and names to identify them easily.
+#' The connections among panels are represented through directed edges.
+#' This can be a compact visualization to obtain an overview for the configuration,
+#' without the need of fully launching the app and loading the content of all
+#' panels
+#'
+#' @details
+#' This function is particularly useful with mid-to-large `initial` objects, as
+#' they can be quickly generated in a programmatic manner via the `iSEEinit()`
+#' provided in this package.
+#'
+#' @param initial An `initial` list object, in the format that is required to
+#' be passed as a parameter in the call to [iSEE::iSEE()].
+#' @param plot_format Character string, one of `igraph`, `visNetwork`, or `none`.
+#' Defaults to `igraph`. Determines the format of the visual representation
+#' generated as a side effect of this function - it can be the output of the
+#' `plot()` function for `igraph` objects, or an interactive widget created
+#' via `visNetwork::visNetwork()`.
+#'
+#' @return An `igraph` object, underlying the visual representation provided.
+#'
 #' @export
+#'
 #' @importFrom igraph graph_from_data_frame graph.empty V V<-
 #' @importFrom visNetwork visNetwork visEdges toVisNetworkData
+#'
+#' @seealso [view_initial_tiles()]
 #'
 #' @examples
 view_initial_network <- function(initial,
