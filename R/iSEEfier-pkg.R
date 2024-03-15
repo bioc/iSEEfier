7280eb399eb6b9934cf8038e5f17349d04da1937
Federico Marini <marinif@uni-mainz.de>
2024-03-15 11:54:12 +0100

    iSEEfier package doc file

---
+++ R/iSEEfier-pkg.R
@@ -0,0 +1,15 @@
+#' iSEEfier: a very convenient way to fire up your iSEE instance
+#'
+#' iSEEfier provides a set of functionality to quickly create, inspect, and
+#' combine `initial` configuration objects.
+#' These can be conveniently passed to the function call to launch `iSEE()`
+#' in this manner.
+#' This currently works with the sets of panels provided by the `iSEE` and `iSEEu`
+#' packages, but can be extended to accommodate the usage of any custom panel
+#' (e.g. from `iSEEde`, `iSEEpathways`, or any panel developed independently by the
+#' user).
+#'
+#' @name iSEEfier-pkg
+#' @docType package
+#' @keywords internal
+"_PACKAGE"
