61746ac2083967657786e7fb342afb9006516fa2
Federico Marini <marinif@uni-mainz.de>
2024-03-17 00:18:50 +0100

    using allen for most test suites

--- tests/testthat/test-iSEEnrich.R
+++ tests/testthat/test-iSEEnrich.R
@@ -1,11 +1,10 @@
 test_that("test iSEEnrich", {
-  sce <- scRNAseq::BacherTCellData()
-  sce <- scuttle::logNormCounts(sce)
-  sce <- scater::runPCA(sce)
+
   GO_collection <- "GO"
   Hs_organism <- "org.Hs.eg.db"
   gene_id <- "SYMBOL"
-  results <- iSEEnrich(sce = sce, collection = GO_collection,
+
+  results <- iSEEnrich(sce = sce_allen, collection = GO_collection,
                        organism = Hs_organism,
                        gene_identifer = gene_id)
   expect_true(class(results)== "list")
