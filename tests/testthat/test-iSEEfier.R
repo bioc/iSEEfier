3242d9194fcc6a6ca1810a41c25f2b3dbec84065
Federico Marini <marinif@uni-mainz.de>
2024-03-17 00:50:26 +0100

    covering extra cases where params are activated

--- tests/testthat/test-iSEEfier.R
+++ tests/testthat/test-iSEEfier.R
@@ -4,5 +4,22 @@ test_that("test iSEEinit",{
                                        "Klre1"),
                       clusters = "Primary.Type",
                       groups = "Secondary.Type")
-  expect_true(class(initial)== "list")
+  expect_true(is.list(initial))
+
+  initial_with_board <- iSEEinit(sce = sce_allen,
+                                 feature.list = c("IL2rb",
+                                                  "Klre1"),
+                                 clusters = "Primary.Type",
+                                 groups = "Secondary.Type",
+                                 markdownboard = TRUE)
+  expect_true(is.list(initial_with_board))
+
+  initial_with_dynamictable <- iSEEinit(sce = sce_allen,
+                                        reddim.type = "PCA",
+                                        feature.list = c("IL2rb",
+                                                         "Klre1"),
+                                        clusters = "Primary.Type",
+                                        groups = "Secondary.Type",
+                                        dynamicMarkerTable = TRUE)
+  expect_true(is.list(initial_with_dynamictable))
 })
