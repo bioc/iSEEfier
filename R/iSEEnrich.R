ccf4e8d8d66ef399917163149fa1352529330776
Najla Abassi <abassi.nejla96@gmail.com>
2024-09-19 16:15:13 +0200

    add a couple of param in iSEEnrich to accomodate more plots for visualization

--- R/iSEEnrich.R
+++ R/iSEEnrich.R
@@ -11,7 +11,17 @@
 #'   extract mappings of gene sets to gene IDs.
 #' @param gene_identifier A character string specifying the identifier to use to
 #'   extract gene IDs for the organism package
-#'
+#'  
+#' @param clusters A character string containing the name of the
+#'   clusters/cell-type/state...(as listed in the colData of the sce)
+#'   
+#' @param groups A character string of the groups/conditions...(as it appears in
+#'   the colData of the sce)
+#'   
+#' @param reddim_type A string vector containing the dimensionality reduction
+#'   type
+#'   
+#'   
 #' @return A list of "Panel" objects specifying the initial state of iSEE
 #'   instance
 #' @export iSEEnrich
@@ -29,15 +39,24 @@
 #' GO_collection <- "GO"
 #' Mm_organism <- "org.Mm.eg.db"
 #' gene_id <- "SYMBOL"
+#' clusters <- "Primary.Type"
+#' groups <- "Secondary.Type"
+#' reddim_type <- "PCA"
 #' results <- iSEEnrich(sce = sce,
 #'                      collection = GO_collection,
 #'                      organism = Mm_organism,
-#'                      gene_identifier = gene_id)
+#'                      gene_identifier = gene_id,
+#'                      clusters = clusters,
+#'                      groups = groups,
+#'                      reddim_type = reddim_type)
 #' 
 iSEEnrich <- function(sce,
                       collection = c("GO", "KEGG"),
                       organism = "org.Hs.eg.db",
-                      gene_identifier = "ENTREZID") {
+                      gene_identifier = "ENTREZID",
+                      clusters = colnames(colData(sce))[1],
+                      groups = colnames(colData(sce))[1],
+                      reddim_type = "PCA") {
 
   ## Checks on arguments
   if (!is(sce, "SingleCellExperiment"))
@@ -50,6 +69,10 @@ iSEEnrich <- function(sce,
   stopifnot(isScalarCharacter(organism))
 
   stopifnot(isScalarCharacter(gene_identifier))
+  
+  stopifnot(isScalarCharacter(clusters))
+  
+  stopifnot(isScalarCharacter(groups))
 
   if (!requireNamespace(organism, quietly = TRUE))
     stop("Please check the value of the provided orgDb package ",
@@ -64,11 +87,45 @@ iSEEnrich <- function(sce,
                                 organism = organism,
                                 identifier = gene_identifier)
   sce1 <- registerFeatureSetCommands(sce, cmds)
-
-  initial[["FeatureSetTable1"]] <- FeatureSetTable()
-
+  
+  initial[["FeatureSetTable1"]] <- FeatureSetTable(Collection = collection)
+  
   initial[["RowDataTable1"]] <- RowDataTable(
     RowSelectionSource = "FeatureSetTable1")
+  
+  initial[["FeatureAssayPlot1"]] <- new(
+    "FeatureAssayPlot",
+    XAxis = "Column data",
+    XAxisColumnData = groups,
+    YAxisFeatureSource = "RowDataTable1",
+    ColorBy = "Column data",
+    ColorByColumnData = groups,
+    ColumnSelectionSource = "ColumnDataPlot1")
+  
+  initial[["ColumnDataPlot1"]] <- new(
+    "ColumnDataPlot",
+    XAxis = "Column data",
+    YAxis = clusters,
+    XAxisColumnData = groups,
+    ColorByColumnData = clusters,
+    ColorBy = "Column data"
+    
+  )
+  
+  initial[["ReducedDimensionPlot1"]] <- new(
+    "ReducedDimensionPlot",
+    Type = reddim_type,
+    FacetColumnByColData = groups,
+    FacetColumnBy = "Column data",
+    ColorBy = "Feature name",
+    ColorByFeatureSource = "RowDataTable1",
+    ColumnSelectionSource = "ColumnDataPlot1"
+  )
+  
+  initial[["MarkdownBoard1"]] <- new(
+    "MarkdownBoard",
+    Content = "# Placeholder\n\nFill me with genes, or anything!")
+  
 
   return(
     list(
